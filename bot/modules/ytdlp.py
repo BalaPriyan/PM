cc4fe91bda132e2d74cea7ac0763a3f7faa5600a
Balapriyan B <131746104+BalaPriyan@users.noreply.github.com>
2023-12-28 00:15:28 +0530

    Update ytdlp.py

--- bot/modules/ytdlp.py
+++ bot/modules/ytdlp.py
@@ -529,6 +529,6 @@ async def ytdlleech(client, message):
 
 
 bot.add_handler(MessageHandler(ytdl, filters=command(
-    BotCommands.YtdlCommand) & CustomFilters.authorized & ~CustomFilters.blacklisted))
+    BotCommands.YtdlCommand) & private))
 bot.add_handler(MessageHandler(ytdlleech, filters=command(
-    BotCommands.YtdlLeechCommand) & CustomFilters.authorized & ~CustomFilters.blacklisted))
+    BotCommands.YtdlLeechCommand) & private))
